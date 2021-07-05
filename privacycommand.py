c572904ed2bb013c2378c41cc7d0d10ca77f80e2
Zachinquarantine <zachinquarantine@yahoo.com>
2021-07-05 13:19:53 -0400

    Create privacycommand.py

---
+++ privacycommand.py
@@ -0,0 +1,3 @@
+@client.command()
+async def privacy(ctx):
+    await ctx.send('We do not require a privacy policy because we store none of your info. If you have any questions, DM the bot owner: Owner#1234.')
